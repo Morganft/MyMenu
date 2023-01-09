d39457b28caa2c3ce81fa1bbf3ab005e8ac518be
Nikita <nikitapodhvatoff@gmail.com>
2023-01-09 23:39:04 +0200

    Add basic view for steps

--- menuproject/menu/admin.py
+++ menuproject/menu/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Receipt, IngredientType, Ingredient
+from .models import Receipt, IngredientType, Ingredient, Step
 
 # Register your models here.
 admin.site.register(Receipt)
 admin.site.register(IngredientType)
 admin.site.register(Ingredient)
+admin.site.register(Step)
